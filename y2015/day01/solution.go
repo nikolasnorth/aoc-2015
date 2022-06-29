691b00265ea32dd5b22e15a94a9363dedb76c48e
nikolasnorth <nikolasnorth1@gmail.com>
2022-06-29 11:09:30 -0400

    Complete 2015: Day 1

--- y2015/day01/solution.go
+++ y2015/day01/solution.go
@@ -36,3 +36,37 @@ func part1(filename string) (int, error) {
 
 	return floor, nil
 }
+
+func part2(filename string) (int, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close()
+
+	floor, pos := 0, 1
+	s := bufio.NewScanner(f)
+	s.Split(bufio.ScanRunes)
+	for s.Scan() {
+		switch s.Text() {
+		case "(":
+			floor++
+		case ")":
+			floor--
+		default:
+			msg := fmt.Sprintf("received unexpected token: %q", s.Text())
+			return 0, errors.New(msg)
+		}
+		if floor == -1 {
+			return pos, nil
+		}
+		pos++
+	}
+
+	err = s.Err()
+	if err != nil {
+		return 0, err
+	}
+
+	return floor, nil
+}
--- y2015/day01/solution_test.go
+++ y2015/day01/solution_test.go
@@ -14,3 +14,14 @@ func TestPart1(t *testing.T) {
 		t.Errorf("expected %d but got %d", expect, result)
 	}
 }
+
+func TestPart2(t *testing.T) {
+	expect := 1795
+	result, err := part2("./input.txt")
+	if err != nil {
+		t.Errorf("got unexpected error: %q", err)
+	}
+	if result != expect {
+		t.Errorf("expected %d but got %d", expect, result)
+	}
+}
